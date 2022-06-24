3232f894a6189769c00febf582384f4d1c1cda89
Katherine Crowson <crowsonkb@gmail.com>
2022-06-24 13:36:08 -0700

    Use a buffer for Fourier Features instead of a learnable parameter

--- k_diffusion/layers.py
+++ k_diffusion/layers.py
@@ -175,7 +175,7 @@ class FourierFeatures(nn.Module):
     def __init__(self, in_features, out_features, std=1.):
         super().__init__()
         assert out_features % 2 == 0
-        self.weight = nn.Parameter(torch.randn([out_features // 2, in_features]) * std)
+        self.register_buffer('weight', torch.randn([out_features // 2, in_features]) * std)
 
     def forward(self, input):
         f = 2 * math.pi * input @ self.weight.T
