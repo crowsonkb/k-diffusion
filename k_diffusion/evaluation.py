ef2cbf1279c1ee7cbdeccd917acfbc17a782f31d
Katherine Crowson <crowsonkb@gmail.com>
2022-08-23 05:42:51 -0700

    Stop computing features for reals if the end of the training set is reached

--- k_diffusion/evaluation.py
+++ k_diffusion/evaluation.py
@@ -53,10 +53,13 @@ class CLIPFeatureExtractor(nn.Module):
 def compute_features(accelerator, sample_fn, extractor_fn, n, batch_size):
     n_per_proc = math.ceil(n / accelerator.num_processes)
     feats_all = []
-    for i in trange(0, n_per_proc, batch_size, disable=not accelerator.is_main_process):
-        cur_batch_size = min(n - i, batch_size)
-        samples = sample_fn(cur_batch_size)[:cur_batch_size]
-        feats_all.append(accelerator.gather(extractor_fn(samples)))
+    try:
+        for i in trange(0, n_per_proc, batch_size, disable=not accelerator.is_main_process):
+            cur_batch_size = min(n - i, batch_size)
+            samples = sample_fn(cur_batch_size)[:cur_batch_size]
+            feats_all.append(accelerator.gather(extractor_fn(samples)))
+    except StopIteration:
+        pass
     return torch.cat(feats_all)[:n]
 
 
