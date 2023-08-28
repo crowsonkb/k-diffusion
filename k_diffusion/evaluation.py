fix mistakes in calculation FID: sqrt root matrix, eps 1e-6, equation

k_diffusion/evaluation.py
@@ -96,39 +96,23 @@ def kid(x, y, max_size=5000):
     return total_mmd / n_partitions
 
 
-class _MatrixSquareRootEig(torch.autograd.Function):
-    @staticmethod
-    def forward(ctx, a):
-        vals, vecs = torch.linalg.eigh(a)
-        ctx.save_for_backward(vals, vecs)
-        return vecs @ vals.abs().sqrt().diag_embed() @ vecs.transpose(-2, -1)
-
-    @staticmethod
-    def backward(ctx, grad_output):
-        vals, vecs = ctx.saved_tensors
-        d = vals.abs().sqrt().unsqueeze(-1).repeat_interleave(vals.shape[-1], -1)
-        vecs_t = vecs.transpose(-2, -1)
-        return vecs @ (vecs_t @ grad_output @ vecs / (d + d.transpose(-2, -1))) @ vecs_t
-
-
 def sqrtm_eig(a):
     if a.ndim < 2:
         raise RuntimeError('tensor of matrices must have at least 2 dimensions')
     if a.shape[-2] != a.shape[-1]:
         raise RuntimeError('tensor must be batches of square matrices')
-    return _MatrixSquareRootEig.apply(a)
+    vals, vecs = torch.linalg.eig(a)
+    return vecs @ vals.sqrt().diag_embed() @ torch.linalg.inv(vecs)
 
 
 @utils.tf32_mode(matmul=False)
-def fid(x, y, eps=1e-8):
+def fid(x, y, eps=1e-6):
     x_mean = x.mean(dim=0)
     y_mean = y.mean(dim=0)
     mean_term = (x_mean - y_mean).pow(2).sum()
     x_cov = torch.cov(x.T)
     y_cov = torch.cov(y.T)
     eps_eye = torch.eye(x_cov.shape[0], device=x_cov.device, dtype=x_cov.dtype) * eps
-    x_cov = x_cov + eps_eye
-    y_cov = y_cov + eps_eye
-    x_cov_sqrt = sqrtm_eig(x_cov)
-    cov_term = torch.trace(x_cov + y_cov - 2 * sqrtm_eig(x_cov_sqrt @ y_cov @ x_cov_sqrt))
+    covmean = sqrtm_eig((x_cov + eps_eye) @ (y_cov + eps_eye)).real
+    cov_term = torch.trace(x_cov + y_cov - 2 * covmean)
     return mean_term + cov_term